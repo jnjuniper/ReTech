1525255cb3dbeb68b2ff7079dc5151e271df7161
amroaltai <amro.altai@hotmail.com>
2025-04-19 23:31:31 +0200

    Uppdaterade databasen efter våra nya produkter, la till nya komponenter, home komponent som används för startsidan, fixade routes efter dom nya komponenterna, la till produktgrid som hämtar produkter från vår databas, fixade så att man kan klicka på respektive produkt för att komma till detaljsidan, detaljsidan fixad enligt wireframe och visar tre liknande produkter som ändras varje gång man uppdaterar sidan, fixade url enligt wireframe

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
-import { HeroComponent} from './components/hero/hero.component';
-
+import { HomeComponent } from './home/home.component';
+import { ProductGridComponent } from './product-grid/product-grid.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 export const routes: Routes = [
-  { path: '', component: HeroComponent},
+  { path: '', component: HomeComponent }, // Startsidan visar HomeComponent
+  { path: 'products', component: ProductGridComponent }, // Separat produktsida
+  { path: 'product/:slug', component: ProductDetailComponent }, // Produktdetalj-sida
+  { path: '**', redirectTo: '' },
 ];
