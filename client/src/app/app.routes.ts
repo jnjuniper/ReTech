873aedbaadb26841e9ae6df059c4bcaae680f8ea
Anton Lundin <lundin09@gmail.com>
2025-04-22 00:54:40 +0200

    Added admin-add page.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,33 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductGridComponent } from './product-grid/product-grid.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Startsidan visar HomeComponent
-  { path: 'products', component: ProductGridComponent }, // Separat produktsida
-  { path: 'product/:slug', component: ProductDetailComponent }, // Produktdetalj-sida
-  { path: '**', redirectTo: '' },
+  
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductGridComponent },
+      { path: 'product/:slug', component: ProductDetailComponent },
+      // {
+      //   path: 'search',
+      //   loadComponent: () =>
+      //     import('./search-results/search-results.component').then(
+      //       (m) => m.SearchResultsComponent
+      //     )
+      // }, Bortkommenterat nu, aktiverar detta när search-results.component är mergad. /AL
+    ]
+  },
+
+  
+  {
+    path: 'admin/products/new',
+    loadComponent: () =>
+      import('./admin/admin.component').then((m) => m.AdminComponent)
+  },
+
+  { path: '**', redirectTo: '' }
 ];
