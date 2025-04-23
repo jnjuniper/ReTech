b157f46aec186f08a570e268f65b357ec0f39e15
Jonathan <168919546+jnjuniper@users.noreply.github.com>
2025-04-23 10:16:15 +0200

    Added info-icons

--- client/src/app/lucide-icons/lucide-icons.module.ts
+++ client/src/app/lucide-icons/lucide-icons.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
-import { Heart, ShoppingCart } from 'lucide-angular';
+import { Heart, ShoppingCart, Globe, Plane, Shield, Smile } from 'lucide-angular';
 
 @NgModule({
-  imports: [LucideAngularModule.pick({ Heart, ShoppingCart })],
+  imports: [LucideAngularModule.pick({ Heart, ShoppingCart, Globe, Shield, Plane, Smile })],
   exports: [LucideAngularModule]
 })
 export class LucideIconsModule {}
